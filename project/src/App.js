625c59ff46132d89f33512685ff5daa9d953e851
pawhewitt <pawhewitt@gmail.com>
2019-06-13 15:12:16 +0100

    ukhjggk

--- project/src/App.js
+++ project/src/App.js
@@ -12,6 +12,7 @@ function App() {
         <header className="App-header">
             <img src={logo} className="App-logo" alt="logo" />
             <img src={logo} className="App-logo" alt="logo" />
+            <img src={logo} className="App-logo" alt="logo" />
         </header>
 
         <section>
