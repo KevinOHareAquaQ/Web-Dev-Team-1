a8e61169df433631b73caefce9049602b1c676c0
KevinOHareAquaQ <50633285+KevinOHareAquaQ@users.noreply.github.com>
2019-06-13 14:22:24 +0100

    test commit

---
+++ project/src/App.js
@@ -0,0 +1,70 @@
+import React from 'react';
+import './App.css';
+import logo from './aquaqlogo.jpeg';
+import BarChart from "./Barchart";
+
+function App() {
+
+  return (
+
+    <div className="App">
+
+        <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+        </header>
+
+        <section>
+
+            <nav>
+                <div className="btn-group">
+                    <button>RDB</button>
+                    <button>HDB</button>
+                </div>
+            </nav>
+
+            <table>
+
+                <tr>
+                    <button>BTN1</button>
+                    <button>BTN2</button>
+                </tr>
+
+                <tr>
+                    <td>
+                        Graph 1
+                    </td>
+
+                    <td rowSpan="2">
+                        Right column
+                    </td>
+                </tr>
+
+                <tr>
+                    <td>
+                        Graph 2
+                    </td>
+                </tr>
+
+            </table>
+        </section>
+
+        <table>
+
+            <tg>
+                <BarChart color={'green'} width={'700'}/>
+            </tg>
+
+            <tg>
+                <BarChart color={'blue'} width={'900'}/>
+            </tg>
+
+        </table>
+
+
+
+
+    </div>
+  );
+}
+
+export default App;
