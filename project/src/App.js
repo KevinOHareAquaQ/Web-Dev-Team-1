246e6e950f449149bbce6813c6bb7164aa261082
smcginn <samuel.mcginn@aquaq.co.uk>
2019-06-25 15:24:15 +0100

    all HDB charts added and Appjs added

--- project/src/App.js
+++ project/src/App.js
@@ -6,6 +6,9 @@ import 'react-dropdown/style.css'
 import Grid from "./Grid";
 import Tabs from './Tabs';
 import DropMenu from "./DropDown";
+import Maxvolchart from "./Maxvolchart";
+import HDBGrid from "./hdbweekmaxmin";
+import HDBGrid2 from "./hdbweeksym";
 
 require('./index.css');
 
@@ -18,11 +21,18 @@ function App() {
         </header>
         <Tabs>
             <div label="RDB">
+            <h1>Price (Running average)</h1>
                 <DropMenu/>
+                <h1>Min price/Max price/LVC per sym</h1>
                 <Grid />
+                <h1>Highest traded sym by vol</h1>
+                <Maxvolchart />
             </div>
             <div label="HDB">
-                <Grid />
+            <h1>Max volume traded per day (previous week)</h1>
+            <HDBGrid />
+            <h1>Volume traded per sym (previous week)</h1>
+            <HDBGrid2 />
             </div>
         </Tabs>
     </div>
---
+++ project/src/hdbweekmaxmin.js
@@ -0,0 +1,80 @@
+import React, {Component} from 'react';
+import {AgGridReact} from 'ag-grid-react';
+import 'ag-grid-community/dist/styles/ag-grid.css';
+import 'ag-grid-community/dist/styles/ag-theme-blue.css';
+import axios from 'axios';
+
+class HDBGrid extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            columnDefs: [{
+                headerName: "MAX DATE", field: "maxdate",sortable:true,filter:true
+            }, {
+                headerName: "MAX VOL", field: "maxcount",sortable:true,filter:true
+            }, {
+                headerName: "MIN DATE", field: "mindate",sortable:true,filter:true
+            }, {
+                headerName: "MIN VOL", field: "mincount",sortable:true,filter:true
+            },
+
+            ],
+            rowData: []
+        }
+        this.updateData();
+    }
+
+    options = {
+        url: 'https://81.137.196.157:8191/executeQuery',
+        auth: {
+            username: 'user',
+            password: 'pass',
+        },
+
+        method: 'post',
+        dataType: 'json',
+        headers:
+            {
+                'Content-Type' : 'application/json',
+                'Accept' : 'application/json',
+                'Authorization' : 'BASIC dXNlcjpwYXNz'
+            }
+    };
+
+    getData(query) {
+        this.options['data'] = { 'query': query, 'response': 'true', 'type': 'sync'};
+        return axios(this.options)
+            .then(response => response.data);
+    }
+
+    updateData() {
+        this.getData("select maxdate:last date,maxcount:last x,mindate:first date,mincount:first x from (asc select count i by date from trade where date>.z.d-7)")
+            .then(data => {
+                if (data.success) {
+                    console.log("data success=true");
+                    this.setState({rowData: data.result});
+                }
+            });
+    }
+
+    componentDidMount() {this.interval= setInterval(() =>  this.updateData(), 5000);}
+
+    render(){
+        return (
+            <div
+                className="ag-theme-blue"
+                style={{
+                    height: '55px',
+                    width: '802px' }}
+            >
+                <AgGridReact
+                    columnDefs={this.state.columnDefs}
+                    rowData={this.state.rowData}>
+                </AgGridReact>
+            </div>
+        );
+    }
+}
+
+export default HDBGrid;
