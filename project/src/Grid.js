d9b9ec9fa12903f581f47a9e6cf236b5327a6965
CMcKeeAquaQ <cameron.mckee@aquaq.co.uk>
2019-06-26 12:58:13 +0100

    colour lvc
    Added basic colour functionality to LVC column

--- project/src/Grid.js
+++ project/src/Grid.js
@@ -12,11 +12,22 @@ class Grid extends Component {
             columnDefs: [{
                 headerName: "SYM", field: "sym",sortable:true,filter:true
             }, {
-                headerName: "MIN", field: "price",sortable:true,filter:true
+                headerName: "MIN", field: "minprice",sortable:true,filter:true
             }, {
-                headerName: "MAX", field: "price",sortable:true,filter:true
+                headerName: "MAX", field: "maxprice",sortable:true,filter:true
             }, {
-                headerName: "LVC", field: "price",sortable:true,filter:true
+                headerName: "LVC", field: "lvc",sortable:true,filter:true, cellClass: "lvcClass", cellStyle: function(params) {
+                    var colour = params.node.data.colour;
+                    console.log({"params": params, "data": params.data, "colour": colour});
+                    if (colour == "red") {
+                        var bGroundClr = '#FE5F5F'
+                    } else if (colour == "green") {
+                        bGroundClr = '#61FE80'
+                    }
+                    return {background: bGroundClr}
+                }
+            }, {
+                headerName: "Colour", field: "colour", sortable:true,filter:true,hide:true
             },
 
             ],
@@ -49,7 +60,7 @@ class Grid extends Component {
     }
 
     updateData() {
-        this.getData("select price:count i by sym from trade")
+        this.getData("?[`trade;();(enlist `sym)!enlist `sym;`maxprice`minprice`lvc!((max;`price);(min;`price);(last;`price))],'([]colour:{?[x[;0]<x[;1];`green;?[x[;0]=x[;1];`same;`red]]}value exec -2#price by sym from trade)")
             .then(data => {
                 if (data.success) {
                     console.log("data success=true");
@@ -66,7 +77,7 @@ class Grid extends Component {
                 className="ag-theme-blue"
                 style={{
                     height: '200px',
-                    width: '800px' }}
+                    width: '825px' }}
             >
                 <AgGridReact
                     columnDefs={this.state.columnDefs}
